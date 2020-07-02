e8cb5042e2c8c9d859c9b061c98b43f73f097cf5
Adek Impianna <adek.impianna@gmail.com>
2020-07-02 13:40:19 +0200

    Completed 09-block-scope

--- exercises/09-block-scope/1-missing-values/1.js
+++ exercises/09-block-scope/1-missing-values/1.js
@@ -1,9 +1,13 @@
 'use strict';
 
-const a = 3;
-console.assert(a === _, 'Test 1');
+let m = 3;
+console.assert(m === 3, 'Test 1: m');
 {
-  const a = 5;
-  console.assert(a === _, 'Test 2');
+  let l = 10;
+  const m = 5;
+  l = 0;
+  console.assert(m === 5, 'Test 2: m');
+  console.assert(l === 0, 'Test 3: l');
 }
-console.assert(a === _, 'Test 3');
+m = 20;
+console.assert(m === 20, 'Test 4: m');
